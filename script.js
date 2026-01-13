aabbcee50bed461a3ea7e091a8bd450a341453b3
Olutobi <olutobi.akinsinmide@edouardgand.fr>
2026-01-13 11:15:54 +0100

    Modification de source script html et correction de faute de frappe

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function convertir(){
     let euro = document.getElementById("euro").value; 
 
     //Taux au 10/01/2026 : 1 BTC = 77 600 €
-    let taux = 77600
+    let taux = 77600;
 
     //BUG : la formule est incorrecte 
     let bitcoin = euro/taux;
 
-    document.getElementById("resultat").texteContent = euro + "€ =" + bitcoin + "BTC"; 
+    document.getElementById("resultat").textContent = euro + "€ =" + bitcoin + "BTC"; 
 }
